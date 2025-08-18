990ae03711eefa46aa37d58c07935159a3a7b03e
Sunniybeliy <sunves080706@gmail.com>
2025-08-18 11:43:11 +0500

    Working commit
    I made working admin page

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import './style.css'
+
+const finishButton = document.getElementById( "finish" )
+const question = document.getElementById( "sovol" )
+const option1 = document.getElementById( "first" )
+const option2 = document.getElementById( "second" )
+const option3 = document.getElementById( "third" )
+const bir = document.getElementById( "bir" )
+const ikki = document.getElementById( "ikki" )
+const uch = document.getElementById( "uch" )
+
+const toriJovobArrayi = [ bir, ikki, uch ]
+
+console.log( toriJovobArrayi )
+
+const newQuestion = {}
+
+finishButton.onclick = () => {
+
+  newQuestion.question = sovol.value
+  newQuestion.options = []
+  newQuestion.options.push( {answer:option1.value}, {answer:option2.value}, {answer:option3.value} )
+  for( let i = 0; i < toriJovobArrayi.length; i++ ){
+    if (toriJovobArrayi[i].checked === true) {
+      newQuestion.correctIndex = i
+    }
+  }
+
+fetch("http://localhost:3000/questions", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(newQuestion)
+})
+alert( "Sovollar qo'shildi" )
+}
+
+
